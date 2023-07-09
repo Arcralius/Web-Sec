d470ef70f03860bdc14a68257002150e8ed1aa9e
Arcralius <owq111@gmail.com>
2023-07-09 17:07:39 +0800

    added counter update in script for log file

--- unqurantine/quarantine.py
+++ unqurantine/quarantine.py
@@ -47,6 +47,32 @@ def delete_file(file_path):
     except OSError as e:
         print(f"Error: Failed to delete the file '{file_path}'. {e}")
 
+def remove_line_from_file(file_path, line_number):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    if line_number < 1 or line_number > len(lines):
+        print(f"Error: Invalid line number '{line_number}'.")
+        return
+
+    del lines[line_number - 1]
+
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+
+    print(f"Line {line_number} has been removed from the file '{file_path}'.")
+
+def update_first_line(file_path, new_content):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    lines[0] = new_content + '\n'
+
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+
+    print(f"The first line of '{file_path}' has been updated.")
+
 # Example usage
 file_path = 'test.conf'  # Replace with the path to your file
 
@@ -56,6 +82,7 @@ if lines > 1:
     x = 2
     while x != lines + 1:
         line_contents = split(get_line_by_index(file_path, x))
+        print(line_contents)
         print_file_contents(line_contents[2])
         user_response = ask_yes_no_prompt(get_last_string_before_slash(line_contents[0]))
 
@@ -64,4 +91,7 @@ if lines > 1:
             delete_file(line_contents[0])
         else:
             delete_file(line_contents[0])
+
+        remove_line_from_file(file_path, x)
+        update_first_line(file_path, str(int(lines) - 2))
         x = x + 1
