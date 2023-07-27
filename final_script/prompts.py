f5c9792efaf794986ac5270449dcf906c41da5c7
Arcralius <owq111@gmail.com>
2023-07-27 10:17:35 +0800

    fixed stuff

--- final_script/prompts.py
+++ final_script/prompts.py
@@ -5,4 +5,4 @@ file_directory = os.path.abspath(__file__)
 file_directory = "/".join(file_directory.split("/")[:-1])
 
 
-print("\nThere are files that are sus, please run 'python " + file_directory + "/quarantine.py'")
+print("\nThere are NPM packages that are deemed suspicious, please run 'python3 or python " + file_directory + "/quarantine.py'")
